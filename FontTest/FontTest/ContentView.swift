b24b6402bd96f188b5efbbebb66680ba50186d92
esteban <estebanpiro113@gmail.com>
2022-02-09 12:44:12 -0400

    NewFonts

--- FontTest/FontTest/ContentView.swift
+++ FontTest/FontTest/ContentView.swift
@@ -13,8 +13,18 @@ struct ContentView: View {
             .font(Font.custom("Roboto-Bold", size: 20))
             .padding()
         Text("Hello, world!2")
-            .font(Font.custom("ProximaNova-Bold Italic", size: 20))
+            .font(Font.custom("Proxima Nova Bold", size: 20))
             .padding()
+        Text("Hello, world!3")
+            .font(Font.custom("Proxima Nova It", size: 20))
+            .padding()
+        Text("Hello, world!4")
+            .font(Font.custom("Proxima Nova Semibold", size: 20))
+            .padding()
+        Text("Hello, world!5")
+            .font(Font.custom("Proxima Nova", size: 20))
+            .padding()
+       
     }
 }
 
