74d23b78f54ad7d95a4677fabbdacf1fa3968bf8
esteban <estebanpiro113@gmail.com>
2022-02-11 14:08:30 -0400

    Implemented Code

--- SplashView/SplashView/LoginScreen.swift
+++ SplashView/SplashView/LoginScreen.swift
@@ -14,23 +14,52 @@ struct LoginScreen: View {
     @State private var shouldShowLoginAlert: Bool = false
 
     var body: some View {
+        LogoImage()
         VStack(alignment: .center)  {
             Text("Log into Gold Plus Rewards.")
 
             TextField("Member ID or username", text: $username)
                 .padding(EdgeInsets(top: 20, leading: 24, bottom: 12, trailing: 24))
 
-            Text("Your username: \(username)")
-
             SecureField("Password", text: $password)
                 .padding(EdgeInsets(top: 20, leading: 24, bottom: 12, trailing: 24))
+            
+           
+                    
+                    Button(action: {print("Button Tapped")}) {
+                        LogingButtonContent()
+                }
+            }
+           
+            }
+            
 
         }
-    }
+  
 
-}
+
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
         LoginScreen()
+    }
+}
+
+struct LogoImage: View {
+    var body: some View {
+        Image("logo_primary")
+    
+        
+    }
+}
+
+struct LogingButtonContent: View {
+    var body: some View {
+        Text("Log In")
+            .font(.headline)
+            .foregroundColor(.black)
+            .padding()
+            .frame(width: 300, height: 40)
+            .background(Color.yellow)
+            .cornerRadius(5.0)
     }
 }
